9fbdd1e85f51b7db31c6af599aacfecc1eca1518
Benjamin Vermot <vermotbenjamin@gmail.com>
2021-05-05 11:39:33 +0200

    Premier Repo !!! WOUHOUUUU

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,5 @@
 let saveButton;
+let timer = 40;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,16 +7,50 @@ function setup() {
   saveButton.position(width - 50, height - 25);
   saveButton.mousePressed(downloadImage);
   background(0);
+
+  setInterval(spawnGomme, 2000);
+  setInterval(addSecondToTimer, 1000);
+  
 }
 
 function draw() {
   fill(0);
   rect(width - 50, height - 25, 50, 25);
-  stroke(255);
+  stroke(255, 50,50);
+
+  drawing();
+  AfficherTimer();
+
+  
+}
+
+function drawing(){
   if (mouseIsPressed) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY, 6);
   }
 }
+
+function AfficherTimer(){
+  textSize(100);
+  textAlign(CENTER, CENTER);
+  fill(255,255,255);
+  noStroke();
+  text(timer, width/2, height/10 );
+}
+
+function spawnGomme(){
+  fill(255,255,255);
+  rectMode(CENTER);
+  noStroke();
+  translate(random(-1,1), random(-1,1));
+  rect(random(0, width),random(0, height),100,100);
+  
+}
+
+function addSecondToTimer(){
+  timer = timer-1;
+}
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
